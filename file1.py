1bf28c91244d5f93cf5bc3381299551553394d2c
bala <bala@gmail.com>
2022-11-20 20:31:44 +0530

    second commit

--- file1.py
+++ file1.py
@@ -1,1 +1,113 @@
 line one
+second line
+bout 228,000,000 search results
+github.comGitHub: Let’s build from here · GitHub
+GitHub is where over 94 million developers shape the future of software, together. Contribute to the open source community, manage your Git repositories, review code like a pro, track bugs and features, power your CI/CD and DevOps workflows, and secure code before you commit it.
+
+Results from github.com
+
+Sign In
+GitHub is where people build software. More than 83 million...
+
+Join Github
+GitHub is where people build software. More than 94 million...
+
+Team
+“With GitHub Actions, deployments happen 75 percent...
+
+Enterprise
+GitHub helps enterprises put them all to work—in one place....
+
+Marketplace
+GitHub integrates with hundreds of tools that help you and...
+
+Features
+Get the right tools for the job. Automate your CI/CD and...
+
+Plans
+GitHub Actions and Packages are free for public repositories...
+
+GitHub Support
+GitHub Support is here to help. Learn about GitHub products,...
+
+Explore
+Issue Label Bot automatically labels issues as either a...
+
+Sign up for GitHub
+GitHub is where people build software. More than 94 million...
+
+People also search for
+github logingithub desktop
+github downloadgithub student developer pack
+gitgit download
+github login accountgithub download for windows
+desktop.github.comGitHub Desktop | Simple collaboration from your desktop
+GitHub Desktop Focus on what matters instead of fighting with Git. Whether you're new to Git or a seasoned user, GitHub Desktop simplifies your development workflow. Download for macOS Download for Windows (64bit)
+
+en.wikipedia.org › wiki › GitHubGitHub - Wikipedia
+The GitHub service was developed by Chris Wanstrath, P. J. Hyett, Tom Preston-Werner, and Scott Chacon using Ruby on Rails, and started in February 2008. The company, GitHub, Inc., has existed since 2007 and is located in San Francisco. [20] The shading of the map illustrates the number of users as a proportion of each country's Internet ...
+
+Founded: February 8, 2008; 14 years ago (as Logical Awesome LLC)
+Key people: Mike Taylor (CFO), Erica Brescia (COO)
+Headquarters: San Francisco, California, U.S.
+Industry: Collaborative version control (GitHub), Blog host (GitHub Pages), Package repository (NPM)
+Top stories
+GitHub’s code-writing tool Copilot helps keep developers ‘in the zone’
+GitHub’s code-writing tool Copilot helps keep developers ‘in the zone’
+Copilot’s job is to anticipate what the developer needs and then present some options—all within the editor environment.
+
+Fast Company
+
+2 days ago
+
+Linkedin and GitHub launch free software development courses
+Nairametrics
+
+Linkedin and GitHub launch free software development courses
+
+1 day ago
+
+Where Does India Feature In GitHub’s Grand Scheme?
+Analytics India Magazine
+
+Where Does India Feature In GitHub’s Grand Scheme?
+
+1 week ago
+
+GitHub Codespaces freely available to all GitHub users
+InfoWorld
+
+GitHub Codespaces freely available to all GitHub users
+
+6 days ago
+
+‘Hey, GitHub!’ will let programmers code with just their voice
+The Verge on MSN.com
+
+‘Hey, GitHub!’ will let programmers code with just their voice
+
+2 weeks ago
+
+View all
+github.com › gitGit · GitHub
+git Public Git Source Code Mirror - This is a publish-only repository but pull requests can be turned into patches to the mailing list via GitGitGadget (https://gitgitgadget.github.io/).
+
+People also ask
+What is GitHub and how does it work?
+
+What is GitHub Desktop for Mac?
+
+How many people use GitHub?
+
+What programming language is GitHub written in?
+
+Searches related to github
+github for windows
+github tutorial for beginners
+github definition
+how to push code to github
+github full form
+what is repository in github
+github student pack
+all about github
+12345Next
